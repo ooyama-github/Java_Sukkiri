70f9aab048898ef5b3e20e0159a45bed2a9cc634
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-06 13:12:33 +0000

    整数を格納できる4つの型

--- MyDiary.java
+++ MyDiary.java
@@ -8,3 +8,17 @@ public class MyDiary {
     System.out.println(age);
   }
 }
+
+//　整数を格納できる4つの型
+
+byte glasses;
+glasses = 2;
+
+short age;
+age = 18;
+
+int salary;
+salary = 152000;
+
+long worldPeople;
+worldPeople = 6900000000L;
