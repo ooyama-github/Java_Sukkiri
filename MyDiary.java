0ae9090d201e41c435213c22002f9cd1675899da
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-06 12:56:10 +0000

    変数宣言の文

--- MyDiary.java
+++ MyDiary.java
@@ -1,7 +1,10 @@
 public class MyDiary {
   public static void main (String[] args) {
-    System.out.println("日記ソフト");
     
-    System.out.println("終了します");
+    int age;
+    
+    age = 30;
+    
+    System.out.println(age);
   }
 }
