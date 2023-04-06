d07cef1ccd163617636e9d9e1ef642127bc22dd4
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-06 13:44:05 +0000

    練習問題３を解きました

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
   public static void main (String[] args) {
     
-    int a = 3;
-    int b = 5;
-    int c = a * b;
-    
-    System.out.println("縦幅3横幅5の面積は、" + c);
+    boolean result = true;
+    char character = '駆';
+    double pi = 3.14;
+    long number = 314159265853979L;
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
   }
 }
 
