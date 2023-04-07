9f6e7f9b1b192e44735c3f7f223d709b2c87daad
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-07 09:15:34 +0000

    練習問題2-1

--- Main.java
+++ Main.java
@@ -1,20 +1,10 @@
-// キーボードから１行の入力を受けとる命令
-// String s = new java.util.Scanner(System.in).nextLine();
-
-// キーボードから１つの整数を入力を受け付ける
-// int input = new java.util.Scanner(System.in).nestInt();
-
 public class Main {
   public static void main(String[] args) {
     
-    System.out.println("あなたの名前を入力して下さい。");
+    int x = 5;
+    int y = 10;
     
-    String name = new java.util.Scanner(System.in).nextLine();
-    
-    System.out.println("あなたの年齢を入力して下さい。");
-    
-    int age = new java.util.Scanner(System.in).nextInt();
-    
-    System.out.println("ようこそ、" + age + "歳の" + name + "さん");
-  } 
+    String ans = "x+yは" + (x + y); //このカッコ（）を抜くと答えは変わってしまう
+    System.out.println(ans);
+  }
 }
