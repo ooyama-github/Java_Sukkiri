b26180ba1265fbd912821ec1d16a611a4255f998
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-07 08:12:05 +0000

    改行せずに文字を表示する命令(System.out.print)

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
+// 改行せずに画面に文字を表示する
+// System.out.print(①);
+
 public class Main {
   public static void main(String[] args) {
     
     String name = "おおやま";
-    String message;
-    message = name + "さん、こんにちは";
     
-    System.out.println(message);
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
   }
 }
