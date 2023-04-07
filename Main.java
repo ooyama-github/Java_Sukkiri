f614cad5054cdf2e73e2b853a24228ab7087acf9
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-07 07:22:07 +0000

    インクリメント演算子の使い方に注意（前か後か）

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    int a;
-    a = 100;
+    int a = 10;
+    int b = 10;
     
-    a++;
-    System.out.println(a);
+    System.out.println(++a + 50);
+    System.out.println(b + 50);
   }
 }
