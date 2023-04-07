8c39362085cb1bdbc74ec3460b2c1e2564972af6
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-07 09:08:03 +0000

    キーボードから入力を受け付ける命令

--- Main.java
+++ Main.java
@@ -1,11 +1,20 @@
-// 乱数を発生させる
-// int r = new ava.util.Random().nextInt
+// キーボードから１行の入力を受けとる命令
+// String s = new java.util.Scanner(System.in).nextLine();
+
+// キーボードから１つの整数を入力を受け付ける
+// int input = new java.util.Scanner(System.in).nestInt();
 
 public class Main {
   public static void main(String[] args) {
     
-    int r = new java.util.Random().nextInt(90);
+    System.out.println("あなたの名前を入力して下さい。");
     
-    System.out.println("あなたはたぶん、" + r + "歳ですね？");
-  }
+    String name = new java.util.Scanner(System.in).nextLine();
+    
+    System.out.println("あなたの年齢を入力して下さい。");
+    
+    int age = new java.util.Scanner(System.in).nextInt();
+    
+    System.out.println("ようこそ、" + age + "歳の" + name + "さん");
+  } 
 }
