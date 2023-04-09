f2307f82d4e212a63567ef306e7b53d7d81357d2
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-09 07:54:37 +0000

    様々なfor文を書いてみた

--- Main.java
+++ Main.java
@@ -1,8 +1,19 @@
-public class Main {
-  public static void main(String[] args) {
-    
-    for(int i = 0; i < 3; i++) {
-      System.out.print("現在" + (i + 1) + "週目→");
-    }
-  }
-}
+//ループ変数を1からスタートする
+
+for(int i = 1; i < 10; i++)
+
+//ループ変数を2ずつ増やす
+
+for(int i = 0; i < 10; i += 2)
+
+//ループ変数を10から1ずつ1まで　減らしていく
+
+for(int = 10;  i < 0; i--)
+
+//ループ変数を初期化しない
+
+for(; i < 10; i++)
+
+//繰り返し時の処理を行わない
+
+for(int i = 0; i < 10;)
