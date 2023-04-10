0a00245098bc6ec98e02b20dd7b12710647b175e
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-10 08:30:36 +0000

    配列の作成手順

--- Main.java
+++ Main.java
@@ -1,21 +1,6 @@
 public class Main {
   public static void main(String[] args) {
     
-    System.out.println("【数あてゲーム】");
-    
-    int ans = new java.util.Random().nextInt(10);
-    
-    for (int i = 0; i < 5; i++) {
-      System.out.println("０～９の数字を入力して下さい");
-      int num = new java.util.Scanner(System.in).nextInt();
-      
-      if(num == ans) {
-        System.out.println("アタリ！");
-        break;
-      } else {
-        System.out.println("違います");
-      }
-    }
-    System.out.println("ゲームを終了します");
+    int[] scores = new int[5];
   }
 }
