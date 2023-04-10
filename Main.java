f109bf72bec0ecc4eb7c0bdc7cfa9af22c3ccb34
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-10 08:41:48 +0000

    配列を用いた点数管理プログラム（エラー）

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 public class Main {
   public static void main(String[] args) {
     
-    int[] scores = new int[5];
-    System.out.println(scores[0]);
+    int[] scores = {20, 30, 40, 50, 80};
+    
+    int sum = scores[1] + scores[2] + scores[3] + scores[4] + scores[5];
+    
+    int avg = sum / scores.length;
+    
+    System.out.println("合計点" + sum);
+    System.out.println("平均点" + avg);
   }
 }
