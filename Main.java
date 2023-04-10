f63e04f627b06046f55c08589e10615b0ffbc021
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-10 08:36:20 +0000

    配列は自動的に初期化される

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     
-    int[] scores;
-    
-    scores = new int[5];
-    scores[1] = 30;
-    
-    System.out.println(scores[1]);
+    int[] scores = new int[5];
+    System.out.println(scores[0]);
   }
 }
