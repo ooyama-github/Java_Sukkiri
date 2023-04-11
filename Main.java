2ab8e6109d1484aa92b61ea1541e427abe5f1f9d
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-11 13:30:24 +0000

    拡張for文

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public class Main {
     
     int[] scores = {20, 30, 40, 50, 80};
     
-    for (int i = 0; i < scores.length; i++) {
-      System.out.println(scores[i]);
+    for (int value : scores) {
+      System.out.println(value);
     }
   }
 }
