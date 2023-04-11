be4a55c67af7cc13ca87de6a301b5ab4d850835c
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-11 13:18:30 +0000

    DNAの記号をランダムに表示する

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
-public class Main{
+public class Main {
   public static void main(String[] args) {
     
-    int[] scores = {20, 30, 49, 50, 80};
+    int[] seq = new int[10];
     
-    int count = 0;
+    //塩基配列をランダムに生成
+    for (int i = 0; i < seq.length; i++) {
+      seq[i] = new java.util.Random().nextInt(4);
+    }
     
-    for (int i = 0; i < scores.length; i++) {
-      if (scores[i] >= 50) {
-        count++;
+    //生成した塩素配列の記号を表示
+    for (int i = 0; i < seq.length; i++) {
+      switch (seq[i]) {
+        case 0:
+          System.out.print("A ");
+          break;
+          
+        case 1:
+          System.out.print("T ");
+          break;
+          
+        case 2:
+          System.out.print("G ");
+          break;
+          
+        case 3:
+          System.out.print("C ");
+          break;
       }
     }
-    System.out.println("50点以上の科目の数は：" + count);
   }
 }
