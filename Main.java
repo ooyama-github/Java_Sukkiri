2f2701ad5d40e776eff4be15c4e3e6548ac5eecb
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-11 13:05:08 +0000

    ５０点以上の科目の数を調べる

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
-public class Main {
+public class Main{
   public static void main(String[] args) {
     
-    int[] scores = {20, 30, 40, 50, 80};
-    int sum = 0;
+    int[] scores = {20, 30, 49, 50, 80};
+    
+    int count = 0;
     
     for (int i = 0; i < scores.length; i++) {
-      sum += scores[i];
+      if (scores[i] >= 50) {
+        count++;
+      }
     }
-    
-    int avg = sum / scores.length;
-    
-    System.out.println("合計点" + sum);
-    System.out.println("平均点" + avg);
+    System.out.println("50点以上の科目の数は：" + count);
   }
 }
