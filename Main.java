e02190c74696168d4bfebc1e4d0f76836234eecf
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-12 09:56:09 +0000

    練習4-3

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 public class Main {
   public static void main(String[] args) {
-
-    int[] moneyList = {121902, 8302, 55100};
-
-    for (int i = 0; i < moneyList.length; i++) {
-      System.out.println(moneyList[i]);
-    }
     
-    for (int m : moneyList) {
-      System.out.println(m);
-    }
+    int[] counts = null;
+    float[] heights = {171.3F, 175.0F);
+    
+    System.out.println(counts[1]);
+    System.out.println(heights[2]);
   }
 }
+
+// ５行目：NullPointerException～nullが格納されている配列変数を利用しようとした
+// ６行目：ArrayIndexOutOfBoundsException～添え字の数があっていない
