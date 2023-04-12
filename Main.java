a8eac9fc0c9a6deb80a405f56f9a3f962d6f46cc
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-12 09:48:06 +0000

    練習4-2

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 public class Main {
   public static void main(String[] args) {
+
+    int[] moneyList = {121902, 8302, 55100};
+
+    for (int i = 0; i < moneyList.length; i++) {
+      System.out.println(moneyList[i]);
+    }
     
-    int[] points = new int[4];
-    
-    double[] weights = new double[5];
-    
-    boolean[] answers = new boolean[3];
-    
-    String[] names = new String[3];
-    
+    for (int m : moneyList) {
+      System.out.println(m);
+    }
   }
 }
