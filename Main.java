76bc0a87d4f024a988f39fdef35201640e0744a3
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-12 09:25:13 +0000

    親配列と子配列の要素数を表示

--- Main.java
+++ Main.java
@@ -1,20 +1,9 @@
 public class Main {
   public static void main(String[] args) {
     
-    int[] [] scores = new int[2] [3];
+    int[][] scores = {{40, 50, 60}, {80, 60, 70}};
     
-    scores[0] [0] = 40;
-    
-    scores[0] [1] = 50;
-    
-    scores[0] [2] = 60;
-    
-    scores[1] [0] = 80;
-    
-    scores[1] [1] = 60;
-    
-    scores[1] [2] = 70;
-    
-    System.out.println(scores[1] [1]);
+    System.out.println(scores.length);
+    System.out.println(scores[0].length);
   }
 }
