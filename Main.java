9f4f75e7cc1da821fac43ef5867c2e39295d8bda
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-12 07:58:13 +0000

    実行結果は１ではなく100になる

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-//拡張for文で配列を回す
-//for (要素の型　任意の変数名 ； 配列変数名)
-
 public class Main {
   public static void main(String[] args) {
     
-    int[] scores = {20, 30, 40, 50, 80};
-    
-    for (int value : scores) {
-      System.out.println(value);
-    }
+    int[] arrayA = {1, 2, 3};
+    int[] arrayB;
+    arrayB = arrayA;
+    arrayB[0] = 100;
+    System.out.println(arrayA[0]);
   }
 }
