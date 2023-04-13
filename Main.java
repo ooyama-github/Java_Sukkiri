fd1d3569f02cd425df93dabcfe45d8193948fae1
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-13 14:18:47 +0000

    シンプルなメソッドの定義

--- Main.java
+++ Main.java
@@ -1,16 +1,5 @@
 public class Main {
-  public static void main(String[] args) {
-    
-    int[] numbers = {3, 4, 9};
-    
-    System.out.println("１桁の数字を入力して下さい");
-    
-    int input = new java.util.Scanner(System.in).nextInt();
-    
-    for (int n : numbers) {
-      if (n == input) {
-        System.out.println("アタリ！");
-      }
-    }
+  public static void hello() {
+    System.out.println("湊さん、こんにちは");
   }
 }
