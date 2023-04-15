74e7980bd724a7803718047018a941ac482843c7
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-15 14:34:22 +0000

    引数を使わずに値を渡せないのか？（エラー）

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("メソッドを呼び出します");
     
-    hello("湊");
-    hello("朝香");
-    hello("菅原");
-    System.out.println("メソッドの呼び出しが終わりました");
+    int x = 100;
+    int y = 10;
+    add();
   }
   
-  public static void hello(String name) {
-    System.out.println(name + "さん、こんにちは");
+  public static void add() {
+    
+    int ans = x + y;
+    System.out.println(x + "+" + y + "=" + ans);
   }
 }
