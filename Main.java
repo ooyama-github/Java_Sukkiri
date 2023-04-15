2852141259b8fb8b6e56fb67cdb0665860184ad9
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-15 14:52:08 +0000

    return文の後に処理は書けない（エラー）

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
-public class Main {
-  public static int add(int x, int y) {
-    int ans = x + y;
-    return ans;
-  }
-  
-  public static void main(String[] args) {
-    System.out.println(add(add(10, 20), add(30, 40)));
-  }
+public static int sample() {
+  .
+  .
+  .
+  return 1;
+  int x = 10;
 }
