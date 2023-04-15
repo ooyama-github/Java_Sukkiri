29b9ed0d59de005306dc206eb056c3b21c222e57
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-15 14:50:12 +0000

    戻り値をそのまま使う

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
   }
   
   public static void main(String[] args) {
-    int ans = add(100, 10);
-    System.out.println("100 + 10 = " + ans);
+    System.out.println(add(add(10, 20), add(30, 40)));
   }
 }
