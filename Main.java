0e0eaac965821bdb762cfbe89bcc6d431fdb909f
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-16 18:26:28 +0000

    引数に配列を受け取るメソッドの利用

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
 public class Main {
-  public static int add(int x, int y) {
-    return x + y;
-  }
-  
-  public static int add(int x, int y, int z) {
-    return x + y + z;
+  //int型配列を受け取り、すべての要素を表示するメソッド
+  public static void printArray(int[] array) {
+    for (int element : array) {
+      System.out.println(element);
+    }
   }
   
   public static void main(String[] args) {
-    System.out.println("10+20=" + add(10, 20));
-    System.out.println("10+20+30=" + add(10, 20, 30));
+    int[] array = {1, 2, 3};
+    printArray(array); //配列を
   }
 }
