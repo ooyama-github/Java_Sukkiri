5616122ffd3f12ad49b1bbbdcb2f8d4a27f77892
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-16 18:16:37 +0000

    オーバーロード（引数の数が異なる場合）

--- Main.java
+++ Main.java
@@ -1,38 +1,13 @@
-<<<<<<< HEAD
-public static int sample() {
-  .
-  .
-  .
-  return 1;
-  int x = 10;
-=======
 public class Main {
-  //1つ目のaddメソッド
   public static int add(int x, int y) {
     return x + y;
   }
   
-  //2つ目のaddメソッド
-  public static double add(double x, double y) {
-    return x + y;
-  }
-  
-  //3つ目のaddメソッド
-  public static String add(String x, String y) {
-    return x + y;
+  public static int add(int x, int y, int z) {
+    return x + y + z;
   }
   
   public static void main(String[] args) {
-    
-    System.out.println(add(10, 20));
-    System.out.println(add(3.5, 2.7));
-    System.out.println(add("Hello", "World"));
+    System.out.println("10+20=" + add(10, 20));
   }
 }
-
-//実行結果
-//30
-//6.2
-//HelloWorld
-
-gitでエラーが出たので試験的にコミットします
