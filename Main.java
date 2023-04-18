32c90b72ae8763516f0c1d9326d7a38fe3b00d81
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-18 04:49:02 +0000

    練習5-3

--- Main.java
+++ Main.java
@@ -1,12 +1,18 @@
 public class Main {
   public static void main(String[] args) {
 
-    String title = "プログラミングについて";
     String address = "oyama@jp";
     String text = "現在Javaを勉強中です";
-    email(title, address, text);
+    email(address, text);
   }
 
+  public static void email(String address, String text) {
+    
+    System.out.println(address + "に、以下のメールを送信しました");
+    System.out.println("件名：無題");
+    System.out.println("本文：" + text);
+  }
+  
   public static void email(String title, String address, String text) {
     
     System.out.println(address + "に、以下のメールを送信しました");
