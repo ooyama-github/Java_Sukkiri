1a9715a0f6124e150f9c898116b7d44cb10582e5
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-18 05:27:32 +0000

    練習5-4

--- Main.java
+++ Main.java
@@ -1,22 +1,21 @@
 public class Main {
   public static void main(String[] args) {
-
-    String address = "oyama@jp";
-    String text = "現在Javaを勉強中です";
-    email(address, text);
-  }
-
-  public static void email(String address, String text) {
     
-    System.out.println(address + "に、以下のメールを送信しました");
-    System.out.println("件名：無題");
-    System.out.println("本文：" + text);
+    double triangleArea = calcTriangleArea(10.0, 5.0);
+    System.out.println("三角形の面積：" + triangleArea + "平方cm");
+    double circleArea = calcCircleArea(5.0);
+    System.out.println("円の面積：" + circleArea + "平方cm");
   }
   
-  public static void email(String title, String address, String text) {
+  public static double calcTriangleArea(double bottom, double height) {
     
-    System.out.println(address + "に、以下のメールを送信しました");
-    System.out.println("件名：" + title);
-    System.out.println("本文：" + text);
+    double area = (bottom * height) / 2;
+    return area;
+  }
+  
+  public static double calcCircleArea(double radius) {
+    
+    double area = radius * radius * 3.14;
+    return area;
   }
 }
