44e533e5efb0f2f4a08c3140f8d8dfee5685beb1
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-20 08:47:49 +0000

    練習6-3

--- comment/Kouhan.java
+++ comment/Kouhan.java
@@ -8,6 +8,7 @@ public class Kouhan {
   public static void showMondokoro() throws Exception {
     System.out.println("飛車さん、角さん・もういいでしょう。");
     System.out.println("この紋所がめにはいらぬか！");
+    Thread.sleep(3000);
     Zenhan.doTogame();  //もう一度、とがめる
   }
 }
