c5b0d893e8ec460ac5364bd18f00744d032112b8
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-20 06:24:51 +0000

    CalcLogic.javaに計算処理を追加する

--- Calc.java
+++ Calc.java
@@ -6,12 +6,4 @@ public class Calc {
     int delta = hiku(a, b);
     System.out.println("足すと" + total + "、引くと" + delta);
   }
-  
-  public static int tasu(int a, int b) {
-    return (a + b);
-  }
-  
-  public static int hiku(int a, int b) {
-    return (a - b);
-  }
 }
