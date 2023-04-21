7dad9ec3313955bc29c949f1b16e3db579ab2972
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-21 08:55:12 +0000

    勇者インスタンスを生成

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -3,5 +3,10 @@ public class Main {
     
     //1.勇者を生成
     Hero h = new Hero();
+    
+    //2.フィールドに初期値をセット
+    h.name = "ミナト";
+    h.hp = 100;
+    System.out.println("勇者" + h.name + "を生み出しました！");
   }
 }
