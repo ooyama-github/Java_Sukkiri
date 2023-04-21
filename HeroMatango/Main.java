e1c09f87f2dc069bb20e98cc63411a9ca37b831a
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-21 08:31:59 +0000

    フィールドを宣言すると同時に初期値も設定

---
+++ HeroMatango/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    
+    //（以下の内容をJavaで）
+    //勇者よ、この仮想世界に生まれよ！
+    //お化けキノコよ、この仮想世界に生まれよ！
+    //勇者よ、戦え！
+    //お化けキノコよ、逃げろ！
+  }
+}
