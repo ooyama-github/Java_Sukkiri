0b7d08e9a5017a6016db1ee6c6437304051b5f1d
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-21 08:44:33 +0000

    メソッド「座る」「転ぶ」「逃げる」を追加

--- HeroMatango/Hero.java
+++ HeroMatango/Hero.java
@@ -3,8 +3,30 @@ public class Hero {
   String name;
   int hp;
   
+  //眠る
   public void sleep() {
     this.hp = 100;
     System.out.println(this.name + "は、眠って回復した！");
+  }
+  
+  //座る
+  public void sit(int sec) {
+    this.hp += sec;
+    System.out.println(this.name + "は、" + sec + "秒座った");
+    System.out.println("HPが" + sec + "ポイント回復した");
+  }
+  
+  //転ぶ
+  public void slip() {
+    this.hp -= 5;
+    System.out.pritnln(this.name + "は、転んだ！");
+    System.out.println("5のダメージ！");
+  }
+  
+  //逃げる
+  public void run() {
+    System.out.println(this.name + "は、逃げだした！");
+    System.out.println("GAMEOVER");
+    System.out.println("最終HPは" + this.hp + "でした");
   }
 }
