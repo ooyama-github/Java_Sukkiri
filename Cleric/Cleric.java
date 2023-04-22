370c2ceee9a948dd9ce2e5b3abb217654e56d907
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-22 03:57:51 +0000

    練習8-3

--- Cleric/Cleric.java
+++ Cleric/Cleric.java
@@ -5,4 +5,14 @@ public class Cleric {
   final int Max_HP = 50;
   int mp = 10;
   final int Max_MP = 10;
+  
+  public void selfAid() {
+    
+    System.out.println(this.name + "は、セルフエイドを！");
+    
+    this hp = this Max_HP;
+    this mp -= 5;
+    
+    System.out.println(this.name + "は、HPが最大まで回復した");
+  }
 }
