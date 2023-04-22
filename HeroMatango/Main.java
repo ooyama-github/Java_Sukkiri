80c5ea7f4c10262c903e456c892a0635e49aae3e
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-22 05:09:12 +0000

    勇者と魔法使いを生み出す

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,16 +1,19 @@
 public class Main {
   public static void main(String[] args) {
     
-    Sword s = new Sword();
-    s.name = "炎の剣";
-    s.damage = 10;
+    Hero h1 = new Hero();
+    h1.name = "ミナト";
+    h1.hp = 100;
     
-    Hero h = new Hero();
-    h.name = "ミナト";
-    h.hp = 100;
-    h.sword = s; //swordフィールドに生成済みの剣インスタンス（の番地）を代入
+    Hero h2 = new Hero();
+    h2.name = "アサカ";
+    h2.hp = 100;
     
-    System.out.println("現在の武器は" + h.sword.name); //h(勇者).（の）sword(剣).(の)name(名前)
-    System.out.println(h.name + "は" + h.sword.name + "で攻撃した！");
+    Wizard w = new Wizard();
+    w.name = "スガワラ";
+    w.hp = 50;
+    w.heal(h1); //ミナトを回復させる（HP: 100 → 110）
+    w.heal(h2); //アサカを回復させる（HP: 100 → 110）
+    w.heal(h2); //アサカを回復させる（HP: 110 → ）
   }
 }
