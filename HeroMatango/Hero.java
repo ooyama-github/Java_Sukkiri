c8fc846f048aa53a8c76bb46824e5a385424d5e5
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-22 09:51:37 +0000

    コンストラクタで引数を追加情報として受け取る

--- HeroMatango/Hero.java
+++ HeroMatango/Hero.java
@@ -11,7 +11,8 @@ public class Hero {
     System.out.println("敵に5ポイントのダメージをあたえた！");
   }
   
-  public Hero() {
+  public Hero(String name) {
     this.hp = 100; //hpフィールドを100で初期化
+    this.name = name; //引数の値でnameフィールドを初期化
   }
 }
