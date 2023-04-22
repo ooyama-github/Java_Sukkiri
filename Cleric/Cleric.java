a7eb00258921ac99677369bead7248d91ec4f261
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-22 04:13:22 +0000

    練習8-4

--- Cleric/Cleric.java
+++ Cleric/Cleric.java
@@ -8,11 +8,26 @@ public class Cleric {
   
   public void selfAid() {
     
-    System.out.println(this.name + "は、セルフエイドを！");
+    System.out.println(this.name + "は、セルフエイドを唱えた！");
     
     this hp = this Max_HP;
     this mp -= 5;
     
     System.out.println(this.name + "は、HPが最大まで回復した");
+  }
+  
+  public int pray(int sec) {
+    
+    System.out.println(this.name + "は" + sec + "秒感祈るを行った！")
+    
+    //乱数
+    int recover = new Random().nextInt(3) + sec;
+    
+    //回復量計算
+    int recoverActual = Math.min(this.MAX_MP - this.mp, recover);
+    
+    this.mp += recoverActual;
+    System.out.println("MPが" + recoverActual + "回復した");
+    return recoverActual;
   }
 }
