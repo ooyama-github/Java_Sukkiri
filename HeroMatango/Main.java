429b38ff82abea770d3a2b5cbc65755467761fa8
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-22 04:24:24 +0000

    2つのHero型変数を利用する

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,23 +1,13 @@
 public class Main {
   public static void main(String[] args) {
     
-    Hero h = new Hero(); //勇者を生成し初期化
-    h.name = "ミナト";
-    h.hp = 100;
+    Hero h1;
+    h1 = new Hero();
+    h1.hp = 100;
     
-    Matango m1 = new Matango(); //お化けキノコA（1匹目）を生成し初期化
-    
-    m1.hp = 50;
-    m1.suffix = "A";
-    
-    Matango m2 = new.Matango(); //お化けキノコB(2匹目)を生成し初期化
-    m2.hp = 48;
-    m2.suffix = "B";
-    
-    //冒険の始まり
-    h.slip(); //勇者は転ぶ
-    m1.run(); //お化けキノコAが逃げる
-    m2.run(); //お化けキノコBが逃げる
-    h.run(; //勇者も逃げる)
+    Hero h2;
+    h2 = h1;
+    h2.hp = 200;
+    System.out.println(h1.hp);
   }
 }
