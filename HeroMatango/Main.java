649fa09b27239dcc273b3d57ad50e0e8fb62c293
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-22 04:45:22 +0000

    344ページ最下部チャレンジしてみた

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -3,7 +3,7 @@ public class Main {
     
     Sword s = new Sword();
     s.name = "炎の剣";
-    s.damege = 10;
+    s.damage = 10;
     
     Hero h = new Hero();
     h.name = "ミナト";
@@ -11,5 +11,6 @@ public class Main {
     h.sword = s; //swordフィールドに生成済みの剣インスタンス（の番地）を代入
     
     System.out.println("現在の武器は" + h.sword.name); //h(勇者).（の）sword(剣).(の)name(名前)
+    System.out.println(h.name + "は" + h.sword.name + "で攻撃した！");
   }
 }
