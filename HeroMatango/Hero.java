26afeaa2d6446dfdee5ece31e49a64a4c788b784
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-23 00:29:40 +0000

    コンストラクタのオーバーロード

--- HeroMatango/Hero.java
+++ HeroMatango/Hero.java
@@ -15,4 +15,10 @@ public class Hero {
     this.hp = 100; //hpフィールドを100で初期化
     this.name = name; //引数の値でnameフィールドを初期化
   }
+  
+  public Hero() {
+    
+    this.hp;
+    this.name = "ダミー"; //ダミーの名前を設定する
+  }
 }
