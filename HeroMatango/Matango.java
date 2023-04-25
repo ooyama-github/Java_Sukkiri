8fe036a826d7e0fd967aa60f347eac2dda1d41af
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-25 11:05:56 +0000

    練習10-3

--- HeroMatango/Matango.java
+++ HeroMatango/Matango.java
@@ -1,10 +1,15 @@
 public class Matango {
   
-  int hp;
-  final int level = 10;
+  int hp = 50;
   char suffix;
+  public Matango(char suffix) {
+    this.suffix = suffix;
+  }
   
-  public void run() {
-    System.out.println("お化けキノコは" + this.suffix + "は逃げ出した！");
+  public void attack(Hero h) {
+    
+    System.out.println("キノコ" + this.suffix + "の攻撃");
+    System.out.println("10のダメージ");
+    h.hp -= 10;
   }
 }
