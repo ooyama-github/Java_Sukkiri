fd0778f7890715a2f2d6f9b38af90f7e61fe9543
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-25 09:19:04 +0000

    SuperHeroクラスにun()を再定義する

--- HeroMatango/SuperHero.java
+++ HeroMatango/SuperHero.java
@@ -1,16 +1,17 @@
-public class SuperHero extends Hero { //extends Heroは「基本的にはHeroと同じと宣言」という意味
-
-  boolean flying; //追加したflying
-  public void fly() { //追加したfly()
+public class SuperHero extends Hero {
+  
+  boolean flying; //新規追加したフィールド
+  public void fly() { //新規追加したメソッド
     this.flying = true;
     System.out.println("飛び上がった！");
   }
   
-  public void land() { //追加したland()
+  public void land() { //新規追加したメソッド
     this.flying = false;
     System.out.println("着地した！");
   }
+  
+  public void run() { //親クラスにも定義してあるが、子クラスで再定義（上書き）するメソッド
+    System.out.println(this.name + "は撤退した");
+  }
 }
-
-
-//ポイントは、1行目のextendsです。「class SuperHero extends Hero」という宣言は、「HeroクラスをベースにしてSuperHeroクラスを定義するので、Heroと同じメンバの定義は省略します（違いだけ記述します）」という意味になります。
