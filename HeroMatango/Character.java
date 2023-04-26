6300ab977cf3f61e542008309873a34969dc6e7d
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-26 23:05:28 +0000

    attack()を抽象メソッドとして宣言する

--- HeroMatango/Character.java
+++ HeroMatango/Character.java
@@ -5,6 +5,6 @@ public class Character {
   public void run() {
     System.out.println(this.name + "は逃げ出した");
   }
-  public void attack(Matango m) { //メソッドの中身を空にしておく
+  public  abstract void attack(Matango m) { //メソッドの中身を空にしておく
   }
 }
