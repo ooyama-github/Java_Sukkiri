d46a7b05db4393c8da53ba9c170d8c13fdfbc6e6
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-26 23:20:18 +0000

    抽象メソッドを正しくオーバーライド

--- HeroMatango/Dancer.java
+++ HeroMatango/Dancer.java
@@ -1,8 +1,11 @@
-public class Dancer extends Character { //Characterは抽象クラス
-  
+public class Dancer extends Character {
   public void dance() {
     System.out.println(this.name + "は情熱的に踊った");
   }
   
-  //attack()をオーバーライドし忘れている1
+  public void attack(Matango m) { //親から継承した「詳細未定の attack()」を上書きする
+    System.out.println(this.name + "の攻撃");
+    System.out.println("敵に3ポイントのダメージ");
+    m.hp -= 3;
+  }
 }
