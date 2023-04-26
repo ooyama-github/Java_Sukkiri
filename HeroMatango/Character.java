b5ae68b57485bc7f932c8d9f53fba71a849d46e2
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-26 22:50:04 +0000

    attack()メソッドの中身を空にしておく

--- HeroMatango/Character.java
+++ HeroMatango/Character.java
@@ -2,15 +2,9 @@ public class Character {
   
   String name;
   int hp;
-  //逃げる
   public void run() {
     System.out.println(this.name + "は逃げ出した");
   }
-  
-  //戦う
-  public void attack(Matango m) {
-    System.out.println(this.name + "の攻撃");
-    m.hp -= ??; //ここを記述しようとして手が止まる
-    System.out.println("敵に？？ポイントのダメージをあたえた！");
+  public void attack(Matango m) { //メソッドの中身を空にしておく
   }
 }
