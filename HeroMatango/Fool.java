77fc80aa2d7311c0d1f920b99cbbc4f3d3ce5483
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-27 00:26:45 +0000

    継承と実装を同時に行う

---
+++ HeroMatango/Fool.java
@@ -0,0 +1,13 @@
+public class Fool extends Character implements Human {
+  //Characterからhpやnameなどのフィールドを継承している
+  //Characterから継承した抽象メソッドattack()を実装
+  public void attack(Matango m) {
+    System.out.println(this.name + "戦わず遊んでいる");
+  }
+  
+  //さらにHumanから継承した４つの抽象メソッドを実装
+  public void talk() {...}
+  public void watch() {...}
+  public void hear() {...}
+  public void run() {...}
+}
