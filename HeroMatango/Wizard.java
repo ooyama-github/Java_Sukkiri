f87d68c1d1d039de800410b0ad365e81928e1424
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-27 01:59:08 +0000

    Characterを継承して定義されたWizard２

--- HeroMatango/Wizard.java
+++ HeroMatango/Wizard.java
@@ -1,11 +1,15 @@
-public class Wizard {
+public class Wizard extends Character {
+  int mp;
+  public void attack(Matango m) {
+    System.out.println(this.name + "の攻撃！");
+    System.out.println("敵に3ポイントのダメージ");
+    m.hp -= 3;
+  }
   
-  String name;
-  int hp;
-  
-  public void heal(Hero h) {
-    
-    h.hp += 10;
-    System.out.println(h.name + "のHPを10回復した！");
+  public void fireball(Matango m) {
+    System.out.println(this.name + "は火の玉を放った！");
+    System.out.println("敵に20ポイントのダメージ");
+    m.hp -= 20;
+    this.mp -= 5;
   }
 }
