7b9d8a5e7b44fca77551e7ee43069538ae18b9ae
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-27 02:11:56 +0000

    Wizardクラスを通常の方法でインスタンス化

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,6 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    
-    Weapon w = new Weapon();
+    Wizard w = new Wizard();
+    Matango m = new Matango();
+    w.name = "アサカ";
+    w.attack(m);
+    w.fireball(m);
   }
 }
