757fbe30e8d0fb10ea295f10d7e2f46a47532624
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-27 02:14:40 +0000

    Wizardにfireballを使わせる（エラー）

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,9 +1,11 @@
 public class Main {
   public static void main(String[] args) {
+    
     Wizard w = new Wizard();
+    Character c = w; //Character型の箱に代入
     Matango m = new Matango();
-    w.name = "アサカ";
-    w.attack(m);
-    w.fireball(m);
+    c.name = "アサカ";
+    c.attack(m);
+    c.fireball(m); //この行でエラーが発生する
   }
 }
