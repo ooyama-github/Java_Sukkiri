cab7e846cc64ca066914c5f6dfb796a487e77294
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-28 02:28:59 +0000

    パーティが宿屋に泊まってHPを回復する

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,11 +1,24 @@
 public class Main {
   public static void main(String[] args) {
     
-    Wizard w = new Wizard();
-    Character c = w; //Character型の箱に代入
-    Matango m = new Matango();
-    c.name = "アサカ";
-    c.attack(m);
-    c.fireball(m); //この行でエラーが発生する
+    // Hero2人を作る
+    Hero h1 = new Hero();
+    Hero h2 = new Hero();
+    //Thief1人を作る
+    Thief t1 = new Thief();
+    //Wizard2人を作る
+    Wizard w1 = new Wizard();
+    Wizard w2 = new Wizard();
+    
+    //冒険開始！　まず宿屋に泊まる
+    h1.hp += 50;
+    h2.hp += 50;
+    t1.hp += 50;
+    w1.hp += 50;
+    w2.hp += 50;
   }
 }
+
+// このコードの前提
+// ・HeroやWizard、Thiefは、抽象クラスCharacterを継承している。
+// ・Characterはnameとhpフィールド、attack()とrun()メソッドを持つ。
