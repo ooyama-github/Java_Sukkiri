3aca242d3ac25ec3d91765b8438ae753e45474be
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-28 02:42:24 +0000

    多態性と配列を組み合わせて宿屋に泊まる

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,24 +1,17 @@
 public class Main {
   public static void main(String[] args) {
     
-    // Hero2人を作る
-    Hero h1 = new Hero();
-    Hero h2 = new Hero();
-    //Thief1人を作る
-    Thief t1 = new Thief();
-    //Wizard2人を作る
-    Wizard w1 = new Wizard();
-    Wizard w2 = new Wizard();
+    Character[] c = new Character[5];
     
-    //冒険開始！　まず宿屋に泊まる
-    h1.hp += 50;
-    h2.hp += 50;
-    t1.hp += 50;
-    w1.hp += 50;
-    w2.hp += 50;
+    c[0] = new Hero();
+    c[1] = new Hero();
+    c[2] = new Thief();
+    c[3] = new Wizard();
+    c[4] = new Wizard();
+    
+    //宿屋に泊まる
+    for (Character ch : c) { //1名ずつ順に取り出し
+      ch.hp += 50; //HPを50回復する
+    }
   }
 }
-
-// このコードの前提
-// ・HeroやWizard、Thiefは、抽象クラスCharacterを継承している。
-// ・Characterはnameとhpフィールド、attack()とrun()メソッドを持つ。
