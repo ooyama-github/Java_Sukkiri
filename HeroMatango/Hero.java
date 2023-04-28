73efb1e095b1cc3633dac240896ab90e96a0d3fd
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-04-28 02:49:21 +0000

    Monsterならなんでも攻撃できるattack()メソッド

--- HeroMatango/Hero.java
+++ HeroMatango/Hero.java
@@ -1,15 +1,8 @@
 public class Hero extends Character {
-  public void attack(Matango m) { //お化けキノコ攻撃用
-    System.out.println(this.name + "の攻撃！");
-    System.out.ptintln("敵に10ポイントのダメージをあたえた！");
-    m.hp -= 10;
-  }
-  
-  public void attack(Goblin g) { //ゴブリン攻撃用
+  public void attack(Monster m) { //モンスター攻撃用
+    
     System.out.println(this.name + "の攻撃！");
     System.out.println("敵に10ポイントのダメージをあたえた！");
-    g.hp -= 10;
+    m.hp -= 10;
   }
-  
-  //以下スライム用など続く
 }
