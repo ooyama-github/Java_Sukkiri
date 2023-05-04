c6bcac0f5663ef4581d02ead379e61b369d67516
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-04 14:42:01 +0000

    HeroクラスにgetNameメソッドを追加

--- HeroMatango/Hero.java
+++ HeroMatango/Hero.java
@@ -1,20 +1,7 @@
 public class Hero {
+  private String name;
   
-  private int hp;
-  String name;
-  Sword sword;
-  
-  public void sleep() {
-    this.hp = 100;
-    System.out.println(this.name + "は、眠って回復した！");
-  }
-  
-  private void die() {
-    System.out.println(this.name + "は死んでしまった！");
-    System.out.println("GAME OVERです。");
-  }
-  
-  public void attack(Matango m) {
-    ...
+  public String getName() {
+    return this.name;
   }
 }
