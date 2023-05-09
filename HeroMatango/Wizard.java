df78d92b19b6ede6d1ec93b69be33e9aaee4bb38
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-09 13:11:15 +0000

    13-1

--- HeroMatango/Wizard.java
+++ HeroMatango/Wizard.java
@@ -1,12 +1,12 @@
 public class Wizard {
-  int hp;
-  int mp;
-  String name;
-  Wand wand;
-  void heal(Hero h) {
+  private int hp;
+  private int mp;
+  private String name;
+  private Wand wand;
+  public void heal(Hero h) {
     int basePoint = 10; //基本回復ポイント
-    int recovPoint = (int)(basePoint * this.wand.power);//杖による増幅
-    h.setHp(h.getHp() + recovPoint); //勇者のHPを回復する
+    int recovPoint = (int)(basePoint * this.wand.power); //杖による増幅
+    h.setHp(h.getHP() + recovPoint); //勇者のHPを回復する
     System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
   }
 }
