d9e155f45fcfdc8904e6e0adf739dd177b20f82b
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-10 13:28:58 +0000

    どんなインスタンスでも受け取れるメソッド

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,7 +1,6 @@
 public class Main {
-  public static void main(String[] args) {
-    Object o1 = new Empty();
-    Object o2 = new Hero();
-    Object o3 = "こんにちは";
+  public void printAnything(Object o) {
+    //何型でもいいから、引数を１つ受け取り画面に表示
+    System.out.println(o.toString());
   }
 }
