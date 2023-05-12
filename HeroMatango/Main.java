e36a9e16d6f9a93ae7173c3dc5afee14e5434c85
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-12 09:00:46 +0000

    各インスタンスから静的フィールドを利用可能

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -2,7 +2,10 @@ public class Main {
   public static void main(String[] args) {
     Hero h1 = new Hero();
     Hero h2 = new Hero();
-    System.out.println(h1.hp); //インスタンスh1の箱hpを表示
-    System.out.println(Hero.money); //クラスHeroの箱moneyを表示
+    Hero.money = 100;
+    System.out.println(Hero.money);  //100と表示
+    System.out.println(h1.money); //100と表示
+    h1.money = 300; //h1.moneyに300を代入
+    System.out.println(h2.money);
   }
 }
