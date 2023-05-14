24766eb4d94c849a3ca9d295cc7b154c4a48b43f
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-14 02:02:55 +0000

    splitメソッドを使った文字列の分割

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,9 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < 10000; i++) {
-      sb.append("Java"); //バッファにJavaを追加
+    String s = "abc,def:ghi";
+    String[] words = s.split("[,:]");
+    for (String w : words) {
+      System.out.print(w + "->");
     }
-    String s = sb.toString(); //完成した連結済み文字列を取り出す
   }
 }
