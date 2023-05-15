cb4e2aee06890c5339f7cb4fd5a42e78441c1dcb
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-15 03:15:45 +0000

    6つのint値とDate型の相互変換

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,11 +1,18 @@
-import java.util.Date; //importしておくと便利
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
   public static void main(String[] args) {
-    Date now = new Date(); //現在の日時を取得
-    System.out.println(now);
-    System.out.println(now.getTime());
-    Date past = new Date(1600705425827L);
-    System.out.println(past);
+    Calendar c = Calendar.getInstance();
+    //6つのint値からDateインスタンスを生成
+    c.set(2019,8,22,1,23,45);
+    c.set(Calendar.MONTH, 9); //月を9(10月)に変更
+    Date d = c.getTime();
+    System.out.println(d);
+    //Dateインスタンスからint値を生成
+    Date now = new Date();
+    c.setTime(now);
+    int y = c.get(Calendar.YEAR); //年で取り出す
+    System.out.println("今年は" + y + "年です");
   }
 }
