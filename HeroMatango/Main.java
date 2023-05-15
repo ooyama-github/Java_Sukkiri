0bba6113dd2b1829357b472ce88472d779c611cb
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-15 03:50:38 +0000

    LocalDateTimeの利用例

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -2,25 +2,12 @@ import java.time.*;
 
 public class Main {
   public static void main(String[] args) {
-    //Instantの生成
-    Instant i1 = Instant.now(); //現在日時を取得
-    //Instantとlong値との相互変換
-    Instant i2 = Instant.ofEpochMilli(1600705425827L);
-    long l = i2.toEpochMilli();
-
-    //ZonedDateTimeの生成
-    ZonedDateTime z1 = ZonedDateTime.now(); //現在日時を取得
-    ZonedDateTime z2 = ZonedDateTime.of(2020, 1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo")); //東京時間2020.1.2.3:04分5秒6ナノ秒を指定して取得
-
-    //InstantとZonedDateTimeの相互変換
-    Instant i3 = z2.toInstant();
-    ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
-
-    //ZonedDateTimeの利用方法
-    System.out.println("東京:" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
-    System.out.println("ロンドン:" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
-    if (z2.isEqual(z3)) {
-      System.out.println("これらは同じ瞬間を指しています"); //同じ瞬間の判定には、equals()ではなくisEqual()を使う
-    }
+    //LocalDateTimeの生成方法
+    LocalDateTime l1 = LocalDateTime.now(); //現在日時を取得
+    LocalDateTime l2 = LocalDateTime.of(2022, 1, 1, 9, 5, 0, 0); //2020年1月1日9時5分を指定して取得
+    
+    //LocalDateTimeとZoneDateTimeの相互変換
+    ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+    LocalDateTime l3 = z1.toLocalDateTime();
   }
 }
