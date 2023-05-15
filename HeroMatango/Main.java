5f1765e8e5072c229de1a2cbe54e1b51ae17adfc
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-15 02:51:05 +0000

    replaceAllメソッドを使った文字列の置換

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,9 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     String s = "abc,def:ghi";
-    String[] words = s.split("[,:]");
-    for (String w : words) {
-      System.out.print(w + "->");
-    }
+    String w = s.replaceAll("[beh]", "X");
+    System.out.println(w); //aXc,dXf:gX
   }
 }
