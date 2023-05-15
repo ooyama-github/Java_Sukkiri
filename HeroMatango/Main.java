109af7d68a34d651470ae8d7a71de53c8cba2d4d
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-15 03:05:31 +0000

    現在日時を表示する

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,8 +1,11 @@
+import java.util.Date; //importしておくと便利
+
 public class Main {
   public static void main(String[] args) {
-    long start = System.currentTimeMillis();
-    //ここで何らかの時間がかかる処理
-    long end = System.currentTimeMillis();
-    System.out.println("処理にかかった時間は..." + (end - start) + "ミリ秒でした");
+    Date now = new Date(); //現在の日時を取得
+    System.out.println(now);
+    System.out.println(now.getTime());
+    Date past = new Date(1600705425827L);
+    System.out.println(past);
   }
 }
