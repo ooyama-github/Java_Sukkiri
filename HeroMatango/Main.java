0a92689e5326324b42cb37fc286a21c4160fa950
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-15 03:43:39 +0000

    再コミット

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -7,20 +7,20 @@ public class Main {
     //Instantとlong値との相互変換
     Instant i2 = Instant.ofEpochMilli(1600705425827L);
     long l = i2.toEpochMilli();
-    
+
     //ZonedDateTimeの生成
     ZonedDateTime z1 = ZonedDateTime.now(); //現在日時を取得
     ZonedDateTime z2 = ZonedDateTime.of(2020, 1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo")); //東京時間2020.1.2.3:04分5秒6ナノ秒を指定して取得
-    
+
     //InstantとZonedDateTimeの相互変換
     Instant i3 = z2.toInstant();
     ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
-    
+
     //ZonedDateTimeの利用方法
     System.out.println("東京:" + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
     System.out.println("ロンドン:" + z3.getYear() + z3.getMonth() + z3.getDayOfMonth());
     if (z2.isEqual(z3)) {
-      System.out.println("これらは同じ瞬間を指しています"); //同じ瞬間の判定には、equals()ではなくisEqual()を
+      System.out.println("これらは同じ瞬間を指しています"); //同じ瞬間の判定には、equals()ではなくisEqual()を使う
     }
   }
 }
