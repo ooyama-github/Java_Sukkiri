6ff458135a1e4fda1840d4f279098f8371227562
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-17 06:05:57 +0000

    練習15-1

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -1,15 +1,11 @@
-import java.time.*;
-
 public class Main {
   public static void main(String[] args) {
-    LocalDate d1 = LocalDate.of(2020,1,1);
-    LocalDate d2 = LocalDate.of(2020,1,4);
+    StringBulder sb = new StringBuilder();
+    for (int i = 0; i < 100; i++) {
+      sb.append(i+1).append(",");
+    }
     
-    //3日間を表すPeriodを2通りの方法で生成
-    Period p1 = Period.ofDays(3);
-    Period p2 = Period.between(d1, d2);
-    
-    //d2のさらに3日後を計算する
-    LocalDate d3 = d2.plus(p2);
+    String s = sb.toString();
+    String[] a = s.split(",");
   }
 }
