beae912ae94793b6983f28ba8ccaeb394bd68a78
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-20 09:28:09 +0000

    Setから要素を取り出すと・・・

--- HeroMatango/Main.java
+++ HeroMatango/Main.java
@@ -4,10 +4,11 @@ import java.util.Set;
 public class Main {
   public static void main(String[] args) {
     Set<String> colors = new HashSet<String>();
-    colors.add("赤");
+    colors.add("赤"); //赤青黄の順に格納
     colors.add("青");
     colors.add("黄");
-    colors.add("赤"); //重複して「赤」を格納しようとしても無視される
-    System.out.println("色は" + colors.size() + "種類");
+    for (String s : colors) {
+      System.out.print(s + "→");
+    }
   }
 }
