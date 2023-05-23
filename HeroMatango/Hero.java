5cddabbeb6d3182232d56d56c9f83dec4b1c4cdc
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-05-23 13:22:44 +0000

    練習16-2

--- HeroMatango/Hero.java
+++ HeroMatango/Hero.java
@@ -1,10 +1,8 @@
-public class Hero {
-    String name;
-    int hp;
-    static int money;
-    
-    public static void setRandomMoney() {
-        Hero.money = (int) (Math.random() * 1000);
-        System.out.println(this.name + "たちの所持金を初期化しました");
-    }
+Hero h1 = new Hero("斎藤");
+Hero h2 = new Hero("鈴木");
+List<Hero> heroes = new ArrayList<Hero>();
+heroes.add(h1);
+heroes.add(h2);
+for (Hero h : heroes) {
+    System.out.println(h.getName());
 }
